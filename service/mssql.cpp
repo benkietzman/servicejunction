6bc34c4c21dd46756376b30c408f807b96ee4032
Kietzman, Ben (bk6471) <bk6471@att.com>
2024-03-29 14:08:42 -0500

    Saved work.

--- service/mssql.cpp
+++ service/mssql.cpp
@@ -22,10 +22,13 @@
 #include <iostream>
 #include <list>
 #include <map>
+#include <netdb.h>
 #include <sstream>
 #include <string>
 #include <sybdb.h>
 #include <sybfront.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <vector>
 using namespace std;
 #include <Json>
@@ -48,100 +51,141 @@ int main(int argc, char *argv[])
   loadRequest(requestArray, request);
   if (requestArray.find("User") != requestArray.end() && !requestArray["User"].empty() && requestArray.find("Password") != requestArray.end() && !requestArray["Password"].empty() && requestArray.find("Server") != requestArray.end() && !requestArray["Server"].empty() && ((requestArray.find("Query") != requestArray.end() && !requestArray["Query"].empty()) || (requestArray.find("Update") != requestArray.end() && !requestArray["Update"].empty())))
   {
-    dberrhandle(mssqlHandle);
-    if (dbinit() != FAIL)
+    addrinfo hints, *result;
+    bool bConnected[3] = {false, false, false};
+    int nReturn;
+    string strPort, strServer;
+    stringstream ssServerPort(requestArray["Server"]);
+    getline(ssServerPort, strServer, ':');
+    getline(ssServerPort, strPort, ':');
+    if (strPort.empty())
     {
-      LOGINREC *login;
-      if ((login = dblogin()) != FAIL)
+      strPort = "1433";
+    }
+    memset(&hints, 0, sizeof(addrinfo));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    if ((nReturn = getaddrinfo(strServer.c_str(), strPort.c_str(), &hints, &result)) == 0)
+    {
+      addrinfo *rp;
+      int fdSocket;
+      bConnected[0] = true;
+      for (rp = result; !bConnected[2] && rp != NULL; rp = rp->ai_next)
       {
-        DBPROCESS *dbconn;
-        DBSETLUSER(login, requestArray["User"].c_str());
-        DBSETLPWD(login, requestArray["Password"].c_str());
-        if ((dbconn = dbopen(login, requestArray["Server"].c_str())) != NULL)
+        bConnected[1] = false;
+        if ((fdSocket = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol)) >= 0)
         {
-          if (requestArray.find("Query") != requestArray.end() && !requestArray["Query"].empty())
+          bConnected[1] = true;
+          if (connect(fdSocket, rp->ai_addr, rp->ai_addrlen) == 0)
           {
-            dbcmd(dbconn, requestArray["Query"].c_str());
-            if (dbsqlexec(dbconn) != FAIL)
+            bConnected[2] = true;
+          }
+          close(fdSocket);
+        }
+      }
+      freeaddrinfo(result);
+    }
+    if (bConnected[2])
+    {
+      dberrhandle(mssqlHandle);
+      if (dbinit() != FAIL)
+      {
+        LOGINREC *login;
+        if ((login = dblogin()) != FAIL)
+        {
+          DBPROCESS *dbconn;
+          DBSETLUSER(login, requestArray["User"].c_str());
+          DBSETLPWD(login, requestArray["Password"].c_str());
+          if ((dbconn = dbopen(login, requestArray["Server"].c_str())) != NULL)
+          {
+            if (requestArray.find("Query") != requestArray.end() && !requestArray["Query"].empty())
             {
-              bProcessed = true;
-              if (dbresults(dbconn) == SUCCEED)
+              dbcmd(dbconn, requestArray["Query"].c_str());
+              if (dbsqlexec(dbconn) != FAIL)
               {
-                int nCols = dbnumcols(dbconn);
-                map<int, string> cols;
-                map<string, DBCHAR *> col;
-                for (int i = 1; i <= nCols; i++)
+                bProcessed = true;
+                if (dbresults(dbconn) == SUCCEED)
                 {
-                  string strKey = dbcolname(dbconn, i);
-                  col[strKey] = new DBCHAR[1024];
-                  dbbind(dbconn, i, NTBSTRINGBIND, 0, (BYTE *)col[strKey]);
-                }
-                while (dbnextrow(dbconn) != NO_MORE_ROWS)
-                {
-                  map<string, string> row;
+                  int nCols = dbnumcols(dbconn);
+                  map<int, string> cols;
+                  map<string, DBCHAR *> col;
+                  for (int i = 1; i <= nCols; i++)
+                  {
+                    string strKey = dbcolname(dbconn, i);
+                    col[strKey] = new DBCHAR[1024];
+                    dbbind(dbconn, i, NTBSTRINGBIND, 0, (BYTE *)col[strKey]);
+                  }
+                  while (dbnextrow(dbconn) != NO_MORE_ROWS)
+                  {
+                    map<string, string> row;
+                    for (auto &i : col)
+                    {
+                      row[i.first] = i.second;
+                    }
+                    responseArray.push_back(row);
+                    row.clear();
+                  }
                   for (auto &i : col)
                   {
-                    row[i.first] = i.second;
+                    delete i.second;
                   }
-                  responseArray.push_back(row);
-                  row.clear();
+                  col.clear();
                 }
-                for (auto &i : col)
+                else
                 {
-                  delete i.second;
+                  stringstream ssError;
+                  ssError << "dbresults():  " << gstrError;
+                  strError = ssError.str();
                 }
-                col.clear();
               }
               else
               {
                 stringstream ssError;
-                ssError << "dbresults():  " << gstrError;
+                ssError << "dbsqlexec():  " << gstrError;
                 strError = ssError.str();
               }
             }
-            else
+            else if (requestArray.find("Update") != requestArray.end() && !requestArray["Update"].empty())
             {
-              stringstream ssError;
-              ssError << "dbsqlexec():  " << gstrError;
-              strError = ssError.str();
+              dbcmd(dbconn, requestArray["Update"].c_str());
+              if (dbsqlexec(dbconn) != FAIL)
+              {
+                bProcessed = true;
+              }
+              else
+              {
+                stringstream ssError;
+                ssError << "dbsqlexec():  " << gstrError;
+                strError = ssError.str();
+              }
             }
           }
-          else if (requestArray.find("Update") != requestArray.end() && !requestArray["Update"].empty())
+          else
           {
-            dbcmd(dbconn, requestArray["Update"].c_str());
-            if (dbsqlexec(dbconn) != FAIL)
-            {
-              bProcessed = true;
-            }
-            else
-            {
-              stringstream ssError;
-              ssError << "dbsqlexec():  " << gstrError;
-              strError = ssError.str();
-            }
+            stringstream ssError;
+            ssError << "dbopen():  " << gstrError;
+            strError = ssError.str();
           }
+          dbloginfree(login);
         }
         else
         {
           stringstream ssError;
-          ssError << "dbopen():  " << gstrError;
+          ssError << "dblogin():  " << gstrError;
           strError = ssError.str();
         }
-        dbloginfree(login);
+        dbexit();
       }
       else
       {
         stringstream ssError;
-        ssError << "dblogin():  " << gstrError;
+        ssError << "dbinit():  " << gstrError;
         strError = ssError.str();
       }
-      dbexit();
     }
     else
     {
-      stringstream ssError;
-      ssError << "dbinit():  " << gstrError;
-      strError = ssError.str();
+      strError = "Please provide a valid Server.";
     }
   }
   else if (requestArray.find("User") == requestArray.end() || requestArray["User"].empty())
